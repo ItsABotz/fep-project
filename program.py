dc02fe5c5233213e5a7e7143e207364c4a33c0b3
ItsABotz <72510428+ItsABotz@users.noreply.github.com>
2023-05-19 19:25:48 -0700

    Update program.py

--- program.py
+++ program.py
@@ -19,24 +19,19 @@ def randomword(length):
     return ''.join(random.choice(letters) for i in range(length))
 
 
-def rickgo():
-    if rickroll == True:
+if rickroll == True:
             webbrowser.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
 
 
-def mousemove():
-    if movemouse == True:
-        if movements < 50:
+if movemouse == True:
+        while movements < 50:
             pyautogui.moveTo(random.randint(0,1920), random.randint(0,1080), duration=0.1)
             movements = movements + 1
             print(movements)
-        elif movements == 50:
-            print("done")
 
 
-def appopens():
-    if openapplications == True:
-        if number < maxopen:
+if openapplications == True:
+        while number < maxopen:
             sleep(3)
             pyautogui.hotkey("win")
             sleep(0.2)
@@ -45,12 +40,4 @@ def appopens():
             pyautogui.hotkey("enter")
             sleep(0.3)
             number = number + 1
-        else:
-            print("done")
             
-while True:
-    sleep(pausetime)
-    rickgo()
-    appopens()
-    mousemove()
-    
